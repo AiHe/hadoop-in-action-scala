4d0327acfcecb3f8410dc79986ec523cae8c8328
AiHe <ai.he@ussuning.com>
2015-09-06 11:24:59 -0700

    refactor
    change package name
    
    specify types of MRjob

--- src/main/scala/hia/part2/chapter4/CitationHistogram.scala
+++ src/main/scala/hia/part2/chapter4/CitationHistogram.scala
@@ -1,4 +1,4 @@
-package inaction.part2.chapter4
+package hia.part2.chapter4
 
 import com.twitter.scalding._
 
@@ -6,8 +6,8 @@ import com.twitter.scalding._
  * Created by aihe on 9/6/15.
  */
 class CitationHistogram(args: Args) extends Job(args) {
-  Csv(args("input"), fields = ('citing, 'cited)).read.
-    map(('citing, 'cited) ->('c1, 'c2)) { line: (String, String) => (line._2, line._1) }.
+  Csv(args("input"), fields = ('citing, 'cited), skipHeader = true).read.
+    map(('citing, 'cited) ->('c1, 'c2)) { line: (String, String) => (line._2.toInt, line._1.toInt) }.
     project(('c1, 'c2)).
     groupBy('c1) { group => group.size }.
     discard('c1).
