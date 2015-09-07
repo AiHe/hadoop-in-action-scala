396d87e76ea9216c50e799837e992ee84cd7bb66
AiHe <ai.he@ussuning.com>
2015-09-07 16:46:36 -0700

    3rd commit
    Part2 Chapter4
    
    InnerProduct

---
+++ src/main/scala/hia/part2/chapter4/InnerProduct.scala
@@ -0,0 +1,26 @@
+package hia.part2.chapter4
+
+import com.twitter.scalding._
+
+/**
+ * Created by aihe on 9/6/15.
+ */
+class InnerProduct(args: Args) extends Job(args) {
+  val input1 = Csv(args("input1"), fields = ('key1, 'value)).read
+    .map('value -> 'val1) { value: String => value.toDouble }
+    .discard('value)
+
+  val input2 = Csv(args("input2"), fields = ('key2, 'value)).read
+    .map('value -> 'val2) { value: String => value.toDouble }
+    .discard('value)
+
+
+  input1.joinWithSmaller(('key1, 'key2), input2)
+    .discard('key1)
+    .map(('val1, 'val2) ->('key3, 'prod)) { value: (Double, Double) => (1, value._1 * value._2) }
+    .project(('key3, 'prod))
+    .groupBy('key3) {
+    _.sum[Double]('prod)
+  }.discard('key3)
+    .write(TextLine(args("output")))
+}
