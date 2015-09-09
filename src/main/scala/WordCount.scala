bb73c3a218a8c90773785487045a271b7a1cb4ba
AiHe <ai.he@ussuning.com>
2015-09-08 18:43:47 -0700

    add running timer for WordCount

--- src/main/scala/WordCount.scala
+++ src/main/scala/WordCount.scala
@@ -2,6 +2,7 @@
  * Created by aihe on 9/4/15.
  */
 import com.twitter.scalding._
+import util.TimeUtil
 
 class WordCount(args: Args) extends Job(args) {
   TextLine(args("input"))
@@ -14,4 +15,15 @@ class WordCount(args: Args) extends Job(args) {
     // Lowercase each word and remove punctuation.
     text.toLowerCase.replaceAll("[^a-zA-Z0-9\\s]", "").split("\\s+")
   }
+}
+
+object WordCountRunner extends App {
+
+  import org.apache.hadoop.conf.Configuration
+  import org.apache.hadoop.util.ToolRunner
+
+  val timer = TimeUtil.withTime("WordCount running time") {
+    ToolRunner.run(new Configuration, new Tool, (classOf[WordCount].getName :: args.toList).toArray)
+  }
+  println(s"Execution time: $timer msec")
 }
